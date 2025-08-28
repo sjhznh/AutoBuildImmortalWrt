3c333ec2c2536730f0e0b7c973e440641c338d89
sjhznh <73376190+sjhznh@users.noreply.github.com>
2025-08-28 17:58:12 +0800

    Update build23.sh

--- x86-64/build23.sh
+++ x86-64/build23.sh
@@ -57,6 +57,15 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# 24.10.2
+PACKAGES="$PACKAGES luci-app-zerotier"
+PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
+PACKAGES="$PACKAGES luci-app-autoreboot"
+PACKAGES="$PACKAGES luci-i18n-appfilter-zh-cn"
+PACKAGES="$PACKAGES luci-app-uhttpd-zh-cn"
+PACKAGES="$PACKAGES luci-app-netdata-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 # ======== shell/custom-packages.sh =======
